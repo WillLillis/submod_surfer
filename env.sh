31df85d416c57517a85be1966edc2c264fb01487
Will Lillis <wlillis@ara.com>
2024-11-13 13:25:34 -0500

    fix: make env.sh work with zsh and bash

--- env.sh
+++ env.sh
@@ -1,4 +1,14 @@
-SUBMOD_SURFER_PATH="${BASH_SOURCE[0]%/*}"
+# ZSH doesn't like BASH_SOURCE, and git bash doesn't like $0
+# We can add more checks here as issues with other shells arise
+SHELL_FILE=$(echo "$SHELL" | sed "s/.*\///")
+echo $SHELL_FILE
+if [ "$SHELL_FILE" = "zsh" ]; then
+    SUBMOD_SURFER_PATH="${0%/*}"
+else 
+    SUBMOD_SURFER_PATH="${BASH_SOURCE[0]%/*}"
+fi
+echo $SUBMOD_SURFER_PATH
+
 submod-surfer() {
     cd "$("$SUBMOD_SURFER_PATH/target/release/submod_surfer" "$@")"
 }
